a29114388ecd6fbf621fcbcfd9cb718e6450e13a
behkkkj <brendabangboys@gmail.com>
2024-09-20 11:49:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Turma da Mônica',
+    'Qual o nome do porco do Cascão?',
+    'Chovinista'
+)
+
+criaCartao(
+    'Bob Esponja',
+    'Como se chama a filha do Sr Sirigueijo?',
+    'Pérola'
+)
+
+criaCartao(
+    'Ben 10',
+    'Qual a raça alienígena da qual Gwen tem parte em seu DNA?',
+    'Vaxasauriano'
+)
+
+criaCartao(
+    'Hello Kitty',
+    'Quem criou',
+    'Yuko Shimizu'
+)
